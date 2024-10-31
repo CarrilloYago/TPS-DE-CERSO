7b23f6b9506a78d84a26954640da2db4b0e3279c
CarrilloYago <168773695+CarrilloYago@users.noreply.github.com>
2024-10-31 19:02:43 -0300

    Delete Microservicios directory

--- Microservicios/microservices/user/src/main/java/com/yago_microservices/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.yago_microservices.repository;
-
-import com.yago_microservices.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
